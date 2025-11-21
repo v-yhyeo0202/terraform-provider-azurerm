e70308cee641a261b9bc174cc0d74e259cb7f10f
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-11-21 11:07:32 +0800

    Draft

--- internal/services/machinelearning/machine_learning_workspace_network_outbound_rule_private_endpoint_resource_test.go
+++ internal/services/machinelearning/machine_learning_workspace_network_outbound_rule_private_endpoint_resource_test.go
@@ -425,15 +425,6 @@ resource "azurerm_redis_cache" "test" {
   }
 }
 
-resource "null_resource" "test" {}
-
-resource "time_sleep" "test" {
-  depends_on = [
-    null_resource.test
-  ]
-  create_duration = "10m"
-}
-
 resource "azurerm_machine_learning_workspace_network_outbound_rule_private_endpoint" "test" {
   name                = "acctest-MLW-outboundrule-%[3]s"
   workspace_id        = azurerm_machine_learning_workspace.test.id
