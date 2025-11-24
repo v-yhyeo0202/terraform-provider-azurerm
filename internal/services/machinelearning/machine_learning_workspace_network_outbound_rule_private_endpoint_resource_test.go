af6f2ec1d1d467bfdc813201ea82ed26fa2dcf5b
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-11-24 08:53:42 +0800

    Draft

--- internal/services/machinelearning/machine_learning_workspace_network_outbound_rule_private_endpoint_resource_test.go
+++ internal/services/machinelearning/machine_learning_workspace_network_outbound_rule_private_endpoint_resource_test.go
@@ -386,8 +386,8 @@ func (r WorkspaceNetworkOutboundPrivateEndpointResource) withRedis(data acceptan
 	return fmt.Sprintf(`
 provider "azurerm" {
   features {
-    resource_group {
-      prevent_deletion_if_contains_resources = false
+    application_insights {
+      disable_generated_rule = true
     }
     key_vault {
       purge_soft_delete_on_destroy       = false
