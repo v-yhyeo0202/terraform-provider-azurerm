736f25393286e89541f83a8c3a8157b986e92220
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-11-25 15:41:10 +0800

    Draft

--- internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource.go
+++ internal/services/postgres/postgresql_flexible_server_virtual_endpoint_resource.go
@@ -141,10 +141,7 @@ func (r PostgresqlFlexibleServerVirtualEndpointResource) Create() sdk.ResourceFu
 				Name: &virtualEndpoint.Name,
 				Properties: &virtualendpoints.VirtualEndpointResourceProperties{
 					EndpointType: pointer.To(virtualendpoints.VirtualEndpointType(virtualEndpoint.Type)),
-					Members: &[]string{
-						sourceServerId.FlexibleServerName,
-						replicaServerId.FlexibleServerName,
-					},
+					Members:      &[]string{replicaServerId.FlexibleServerName},
 				},
 			}); err != nil {
 				return fmt.Errorf("creating %s: %+v", sourceEndpointId, err)
