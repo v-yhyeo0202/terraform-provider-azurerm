375d67ea1cc31103b9704b009844ab707c2fa8b2
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-02 22:08:34 +0800

    Draft

--- internal/services/policy/management_group_policy_set_definition_resource_test.go
+++ internal/services/policy/management_group_policy_set_definition_resource_test.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/resources/2025-01-01/policysetdefinitions"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	// "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	// "github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
