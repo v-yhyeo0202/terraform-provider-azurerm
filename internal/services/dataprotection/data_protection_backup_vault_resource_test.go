c8b37b4e8ae8aa02dfdc34002a8a5252f7e3b49d
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-11 12:53:56 +0800

    Draft

--- internal/services/dataprotection/data_protection_backup_vault_resource_test.go
+++ internal/services/dataprotection/data_protection_backup_vault_resource_test.go
@@ -215,7 +215,137 @@ resource "azurerm_resource_group" "test" {
 `, data.RandomInteger, data.Locations.Primary)
 }
 
-func (r DataProtectionBackupVaultResource) completeTemplate(data acceptance.TestData) string {
+/*
+	func (r DataProtectionBackupVaultResource) completeTemplate(data acceptance.TestData) string {
+		template := r.template(data)
+		return fmt.Sprintf(`
+
+%s
+
+data "azurerm_client_config" "current" {}
+
+	resource "azurerm_key_vault" "test" {
+	  name                        = "acctest-key-vault-%s"
+	  location                    = azurerm_resource_group.test.location
+	  resource_group_name         = azurerm_resource_group.test.name
+	  enabled_for_disk_encryption = true
+	  tenant_id                   = data.azurerm_client_config.current.tenant_id
+	  soft_delete_retention_days  = 7
+	  purge_protection_enabled    = true
+
+	  sku_name = "standard"
+
+	  access_policy {
+	    tenant_id = data.azurerm_client_config.current.tenant_id
+	    object_id = data.azurerm_client_config.current.object_id
+
+	    key_permissions = [
+	      "Create",
+	      "Decrypt",
+	      "Encrypt",
+	      "Delete",
+	      "Get",
+	      "List",
+	      "Purge",
+	      "UnwrapKey",
+	      "WrapKey",
+	      "Verify",
+	      "GetRotationPolicy"
+	    ]
+	    secret_permissions = [
+	      "Set",
+	    ]
+	  }
+
+	  access_policy {
+	    tenant_id = azurerm_data_protection_backup_vault.test.identity[0].tenant_id
+	    object_id = azurerm_data_protection_backup_vault.test.identity[0].principal_id
+
+	    key_permissions = [
+	      "Create",
+	      "Decrypt",
+	      "Encrypt",
+	      "Delete",
+	      "Get",
+	      "List",
+	      "Purge",
+	      "UnwrapKey",
+	      "WrapKey",
+	      "Verify",
+	      "GetRotationPolicy"
+	    ]
+	    secret_permissions = [
+	      "Set",
+	    ]
+	  }
+	}
+
+	resource "azurerm_key_vault_key" "test" {
+	  name         = "acctestkey-%s"
+	  key_vault_id = azurerm_key_vault.test.id
+	  key_type     = "RSA"
+	  key_size     = 2048
+
+	  key_opts = [
+	    "decrypt",
+	    "encrypt",
+	    "sign",
+	    "unwrapKey",
+	    "verify",
+	    "wrapKey",
+	  ]
+	}
+
+`, template, data.RandomString, data.RandomString)
+}
+*/
+func (r DataProtectionBackupVaultResource) basic(data acceptance.TestData) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_data_protection_backup_vault" "test" {
+  name                = "acctest-bv-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+  datastore_type      = "VaultStore"
+  redundancy          = "LocallyRedundant"
+}
+`, template, data.RandomInteger)
+}
+
+func (r DataProtectionBackupVaultResource) crossRegionRestore(data acceptance.TestData, enabled bool) string {
+	template := r.template(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_data_protection_backup_vault" "test" {
+  name                         = "acctest-bv-%d"
+  resource_group_name          = azurerm_resource_group.test.name
+  location                     = azurerm_resource_group.test.location
+  datastore_type               = "VaultStore"
+  redundancy                   = "GeoRedundant"
+  cross_region_restore_enabled = %t
+}
+`, template, data.RandomInteger, enabled)
+}
+
+func (r DataProtectionBackupVaultResource) requiresImport(data acceptance.TestData) string {
+	config := r.basic(data)
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_data_protection_backup_vault" "import" {
+  name                = azurerm_data_protection_backup_vault.test.name
+  resource_group_name = azurerm_data_protection_backup_vault.test.resource_group_name
+  location            = azurerm_data_protection_backup_vault.test.location
+  datastore_type      = azurerm_data_protection_backup_vault.test.datastore_type
+  redundancy          = azurerm_data_protection_backup_vault.test.redundancy
+}
+`, config)
+}
+
+func (r DataProtectionBackupVaultResource) complete(data acceptance.TestData) string {
 	template := r.template(data)
 	return fmt.Sprintf(`
 %s
@@ -293,59 +423,6 @@ resource "azurerm_key_vault_key" "test" {
     "wrapKey",
   ]
 }
-`, template, data.RandomString, data.RandomString)
-}
-
-func (r DataProtectionBackupVaultResource) basic(data acceptance.TestData) string {
-	template := r.template(data)
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_data_protection_backup_vault" "test" {
-  name                = "acctest-bv-%d"
-  resource_group_name = azurerm_resource_group.test.name
-  location            = azurerm_resource_group.test.location
-  datastore_type      = "VaultStore"
-  redundancy          = "LocallyRedundant"
-}
-`, template, data.RandomInteger)
-}
-
-func (r DataProtectionBackupVaultResource) crossRegionRestore(data acceptance.TestData, enabled bool) string {
-	template := r.template(data)
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_data_protection_backup_vault" "test" {
-  name                         = "acctest-bv-%d"
-  resource_group_name          = azurerm_resource_group.test.name
-  location                     = azurerm_resource_group.test.location
-  datastore_type               = "VaultStore"
-  redundancy                   = "GeoRedundant"
-  cross_region_restore_enabled = %t
-}
-`, template, data.RandomInteger, enabled)
-}
-
-func (r DataProtectionBackupVaultResource) requiresImport(data acceptance.TestData) string {
-	config := r.basic(data)
-	return fmt.Sprintf(`
-%s
-
-resource "azurerm_data_protection_backup_vault" "import" {
-  name                = azurerm_data_protection_backup_vault.test.name
-  resource_group_name = azurerm_data_protection_backup_vault.test.resource_group_name
-  location            = azurerm_data_protection_backup_vault.test.location
-  datastore_type      = azurerm_data_protection_backup_vault.test.datastore_type
-  redundancy          = azurerm_data_protection_backup_vault.test.redundancy
-}
-`, config)
-}
-
-func (r DataProtectionBackupVaultResource) complete(data acceptance.TestData) string {
-	template := r.completeTemplate(data)
-	return fmt.Sprintf(`
-%s
 
 resource "azurerm_data_protection_backup_vault" "test" {
   name                = "acctest-bv-%d"
@@ -371,13 +448,87 @@ resource "azurerm_data_protection_backup_vault" "test" {
     ENV = "Test"
   }
 }
-`, template, data.RandomInteger)
+`, template, data.RandomString, data.RandomString, data.RandomInteger)
 }
 
 func (r DataProtectionBackupVaultResource) completeUpdate(data acceptance.TestData) string {
-	template := r.completeTemplate(data)
+	template := r.template(data)
 	return fmt.Sprintf(`
 %s
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "test" {
+  name                        = "acctest-key-vault-%s"
+  location                    = azurerm_resource_group.test.location
+  resource_group_name         = azurerm_resource_group.test.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = true
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Create",
+      "Decrypt",
+      "Encrypt",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "UnwrapKey",
+      "WrapKey",
+      "Verify",
+      "GetRotationPolicy"
+    ]
+    secret_permissions = [
+      "Set",
+    ]
+  }
+
+  access_policy {
+    tenant_id = azurerm_user_assigned_identity.test.tenant_id
+    object_id = azurerm_user_assigned_identity.test.principal_id
+
+    key_permissions = [
+      "Create",
+      "Decrypt",
+      "Encrypt",
+      "Delete",
+      "Get",
+      "List",
+      "Purge",
+      "UnwrapKey",
+      "WrapKey",
+      "Verify",
+      "GetRotationPolicy"
+    ]
+    secret_permissions = [
+      "Set",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_key" "test" {
+  name         = "acctestkey-%s"
+  key_vault_id = azurerm_key_vault.test.id
+  key_type     = "RSA"
+  key_size     = 2048
+
+  key_opts = [
+    "decrypt",
+    "encrypt",
+    "sign",
+    "unwrapKey",
+    "verify",
+    "wrapKey",
+  ]
+}
 
 resource "azurerm_user_assigned_identity" "test" {
   resource_group_name = azurerm_resource_group.test.name
@@ -412,7 +563,7 @@ resource "azurerm_data_protection_backup_vault" "test" {
     ENV = "Test"
   }
 }
-`, template, data.RandomInteger, data.RandomInteger)
+`, template, data.RandomString, data.RandomString, data.RandomInteger, data.RandomInteger)
 }
 
 func (r DataProtectionBackupVaultResource) updateIdentityToSystemAssigned(data acceptance.TestData) string {
