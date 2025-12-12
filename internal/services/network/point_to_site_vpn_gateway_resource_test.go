0b219c0dcc1f05c62e7e371eb1bf122816ab00a4
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-12 08:02:54 +0800

    Draft

--- internal/services/network/point_to_site_vpn_gateway_resource_test.go
+++ internal/services/network/point_to_site_vpn_gateway_resource_test.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
-	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/plancheck"
+	// "github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	// "github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
