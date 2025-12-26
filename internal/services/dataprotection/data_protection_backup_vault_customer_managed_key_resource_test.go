0490686668c52f70a7f117ae6159de4934558561
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-26 11:29:21 +0800

    Draft

--- internal/services/dataprotection/data_protection_backup_vault_customer_managed_key_resource_test.go
+++ internal/services/dataprotection/data_protection_backup_vault_customer_managed_key_resource_test.go
@@ -360,7 +360,7 @@ resource "azurerm_data_protection_backup_vault" "test" {
 
   infrastructure_encryption_settings {
     identity_id = azurerm_user_assigned_identity.test.id
-	  key_vault_key_id = azurerm_key_vault_key.test2.id
+	  key_vault_key_id = azurerm_key_vault_key.test.id
     encryption_enabled = true
   }
 }
@@ -369,78 +369,6 @@ data "azurerm_client_config" "current" {}
 
 resource "azurerm_key_vault" "test" {
   name                        = "acctest-key-vault-%s"
-  location                    = azurerm_resource_group.test.location
-  resource_group_name         = azurerm_resource_group.test.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = true
-
-  sku_name = "standard"
-
-  access_policy {
-    tenant_id = data.azurerm_client_config.current.tenant_id
-    object_id = data.azurerm_client_config.current.object_id
-
-    key_permissions = [
-      "Create",
-      "Decrypt",
-      "Encrypt",
-      "Delete",
-      "Get",
-      "List",
-      "Purge",
-      "UnwrapKey",
-      "WrapKey",
-      "Verify",
-      "GetRotationPolicy"
-    ]
-    secret_permissions = [
-      "Set",
-    ]
-  }
-
-  access_policy {
-    tenant_id = azurerm_data_protection_backup_vault.test.identity[0].tenant_id
-    object_id = azurerm_data_protection_backup_vault.test.identity[0].principal_id
-
-    key_permissions = [
-      "Create",
-      "Decrypt",
-      "Encrypt",
-      "Delete",
-      "Get",
-      "List",
-      "Purge",
-      "UnwrapKey",
-      "WrapKey",
-      "Verify",
-      "GetRotationPolicy"
-    ]
-    secret_permissions = [
-      "Set",
-    ]
-  }
-}
-
-resource "azurerm_key_vault_key" "test" {
-  name         = "acctestkey-%s"
-  key_vault_id = azurerm_key_vault.test.id
-  key_type     = "RSA"
-  key_size     = 2048
-
-  key_opts = [
-    "decrypt",
-    "encrypt",
-    "sign",
-    "unwrapKey",
-    "verify",
-    "wrapKey",
-  ]
-}
-
-resource "azurerm_key_vault" "test2" {
-  name                        = "acctest-key-vault2-%s"
   location                    = azurerm_resource_group.test.location
   resource_group_name         = azurerm_resource_group.test.name
   enabled_for_disk_encryption = true
@@ -495,9 +423,9 @@ resource "azurerm_key_vault" "test2" {
   }
 }
 
-resource "azurerm_key_vault_key" "test2" {
-  name         = "acctestkey2-%s"
-  key_vault_id = azurerm_key_vault.test2.id
+resource "azurerm_key_vault_key" "test" {
+  name         = "acctestkey-%s"
+  key_vault_id = azurerm_key_vault.test.id
   key_type     = "RSA"
   key_size     = 2048
 
@@ -516,7 +444,7 @@ resource "azurerm_user_assigned_identity" "test" {
   location            = azurerm_resource_group.test.location
   name                = "acctestBV-%d"
 }
-`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomString, data.RandomString, data.RandomString, data.RandomString, data.RandomInteger)
+`, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomString, data.RandomString, data.RandomInteger)
 }
 
 func (r DataProtectionBackupVaultCustomerManagedKeyResource) userAssignedIdentityBlocked(data acceptance.TestData) string {
