b48995811d37cf14f7a35ea03369d3fe2c045c1f
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2025-12-26 14:51:12 +0800

    Draft

--- internal/services/network/express_route_port_resource_test.go
+++ internal/services/network/express_route_port_resource_test.go
@@ -140,7 +140,7 @@ resource "azurerm_express_route_port" "test" {
   name                = "acctestERP-%d"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
-  peering_location    = "Equinix-London-LDS"
+  peering_location    = "Area51-ERDirect"
   bandwidth_in_gbps   = 10
   encapsulation       = "Dot1Q"
   billing_type        = "MeteredData"
