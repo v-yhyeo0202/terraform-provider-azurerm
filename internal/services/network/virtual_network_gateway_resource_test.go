6190ce8ecf9883e027bca9fa7ab552b6785c9f48
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-02 14:14:50 +0800

    Draft

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1724,7 +1724,7 @@ resource "azurerm_public_ip" "test" {
   name                = "acctestpip-%d"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
   sku                 = "Standard"
 }
 
