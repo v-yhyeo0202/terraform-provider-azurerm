67ca7a8d65c08e59d432feaf2b7f526dcef09c11
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-02 16:58:09 +0800

    Draft

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1743,6 +1743,10 @@ resource "azurerm_virtual_network_gateway" "test" {
   type     = "ExpressRoute"
   vpn_type = "RouteBased"
   sku      = "Standard"
+
+  ip_configuration {
+    subnet_id                     = azurerm_subnet.test.id
+  }
 }
 `, data.RandomInteger, data.Locations.Primary, data.RandomInteger, data.RandomInteger, data.RandomInteger)
 }
