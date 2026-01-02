fc7335bde312e4cd47bfdd05533ca5d18abde76f
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-02 15:29:06 +0800

    Draft

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1711,6 +1711,7 @@ resource "azurerm_virtual_network" "test" {
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   address_space       = ["10.0.0.0/16"]
+  edge_zone = data.azurerm_extended_locations.test.extended_locations[0]
 }
 
 resource "azurerm_subnet" "test" {
