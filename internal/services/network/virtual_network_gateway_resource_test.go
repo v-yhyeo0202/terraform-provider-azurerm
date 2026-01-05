8928e2f73a24a31634f6a37bd4015f839d329b5a
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-05 16:57:38 +0800

    Draft

--- internal/services/network/virtual_network_gateway_resource_test.go
+++ internal/services/network/virtual_network_gateway_resource_test.go
@@ -1842,7 +1842,6 @@ resource "azurerm_subnet" "test" {
   resource_group_name  = azurerm_resource_group.test.name
   virtual_network_name = azurerm_virtual_network.test.name
   address_prefixes     = ["10.0.1.0/24"]
-  default_outbound_access_enabled               = false
 }
 
 resource "azurerm_public_ip" "test" {
