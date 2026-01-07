6185da1541d8c8b26c68354a209533220ac69420
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-07 10:03:32 +0800

    Draft

--- internal/services/apimanagement/api_management_api_resource.go
+++ internal/services/apimanagement/api_management_api_resource.go
@@ -15,6 +15,7 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/apimanagement/2022-08-01/api"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/network/2025-01-01/virtualwans"
 	"github.com/hashicorp/go-azure-sdk/sdk/client/pollers"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -304,7 +305,7 @@ func resourceApiManagementApi() *pluginsdk.Resource {
 									string(api.BearerTokenSendingMethodsAuthorizationHeader),
 									string(api.BearerTokenSendingMethodsQuery),
 								}, false),
-								// ValidateFunc: virtualwans.ValidateNatRuleID,
+								ValidateFunc: virtualwans.ValidateNatRuleID,
 							},
 						},
 					},
