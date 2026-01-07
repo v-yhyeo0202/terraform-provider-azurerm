ca88563db6d99c9a4dfc2a3ca82dfd1cf816fabb
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-07 11:00:35 +0800

    Draft

--- internal/services/network/vpn_gateway_connection_resource.go
+++ internal/services/network/vpn_gateway_connection_resource.go
@@ -156,7 +156,7 @@ func resourceVPNGatewayConnection() *pluginsdk.Resource {
 							Type:     pluginsdk.TypeSet,
 							Optional: true,
 							Elem: &pluginsdk.Schema{
-								Type:         pluginsdk.TypeString,
+								Type: pluginsdk.TypeString,
 								// ValidateFunc: virtualwans.ValidateNatRuleID,
 							},
 						},
@@ -165,7 +165,7 @@ func resourceVPNGatewayConnection() *pluginsdk.Resource {
 							Type:     pluginsdk.TypeSet,
 							Optional: true,
 							Elem: &pluginsdk.Schema{
-								Type:         pluginsdk.TypeString,
+								Type: pluginsdk.TypeString,
 								// ValidateFunc: virtualwans.ValidateNatRuleID,
 							},
 						},
@@ -602,8 +602,8 @@ func flattenVpnGatewayConnectionVpnSiteLinkConnections(input *[]virtualwans.VpnS
 		output = append(output, map[string]interface{}{
 			"name":                                  pointer.From(item.Name),
 			"dpd_timeout_seconds":                   int(pointer.From(props.DpdTimeoutSeconds)),
-			"egress_nat_rule_ids":                   pointer.From(flattenVpnGatewayConnectionNatRuleIds(props.EgressNatRules)),
-			"ingress_nat_rule_ids":                  pointer.From(flattenVpnGatewayConnectionNatRuleIds(props.IngressNatRules)),
+			"egress_nat_rule_ids":                   flattenVpnGatewayConnectionNatRuleIds(props.EgressNatRules),
+			"ingress_nat_rule_ids":                  flattenVpnGatewayConnectionNatRuleIds(props.IngressNatRules),
 			"vpn_site_link_id":                      vpnSiteLinkId,
 			"route_weight":                          int(pointer.From(props.RoutingWeight)),
 			"protocol":                              connectionProtocolType,
