b7fbae7e797cc2af7a7b8fd2492e98ae85657841
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-08 10:31:28 +0800

    Draft

--- internal/services/securitycenter/security_center_subscription_pricing_resource_test.go
+++ internal/services/securitycenter/security_center_subscription_pricing_resource_test.go
@@ -141,7 +141,7 @@ func testAccSecurityCenterSubscriptionPricing_storageAccountDefender(t *testing.
 	})
 }
 
-func testAccSecurityCenterSubscriptionPricing_cloudPostureExtension(t *testing.T) {
+func TestAccSecurityCenterSubscriptionPricing_cloudPostureExtension(t *testing.T) {
 	data := acceptance.BuildTestData(t, "azurerm_security_center_subscription_pricing", "test")
 	r := SecurityCenterSubscriptionPricingResource{}
 
