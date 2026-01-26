f37e6890f5172f4aa1787681d1fc8a491ade1004
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-26 11:14:05 +0800

    Draft

--- internal/services/automation/automation_watcher_resource_test.go
+++ internal/services/automation/automation_watcher_resource_test.go
@@ -11,8 +11,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/hashicorp/go-azure-helpers/lang/pointer"
 	"github.com/hashicorp/go-azure-sdk/resource-manager/automation/2020-01-13-preview/watcher"
-  "github.com/hashicorp/terraform-plugin-testing/helper/resource"
-	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/acceptance/check"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
@@ -40,7 +38,7 @@ func TestAccWatcher_basic(t *testing.T) {
 func TestAccWatcher_update(t *testing.T) {
 	data := acceptance.BuildTestData(t, automation.WatcherResource{}.ResourceType(), "test")
 	r := WatcherResource{}
-	data.ResourceTestIgnoreRecreate(t, r, []acceptance.TestStep{
+	data.ResourceTest(t, r, []acceptance.TestStep{
 		{
 			Config: r.basic(data),
 			Check: acceptance.ComposeTestCheckFunc(
@@ -51,11 +49,13 @@ func TestAccWatcher_update(t *testing.T) {
 		data.ImportStep("tags", "etag", "location"),
 		{
 			Config: r.update(data),
-      ConfigPlanChecks: resource.ConfigPlanChecks{
-				PreApply: []plancheck.PlanCheck{
-					plancheck.ExpectResourceAction(data.ResourceName, plancheck.ResourceActionReplace),
+			/*
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(data.ResourceName, plancheck.ResourceActionReplace),
+					},
 				},
-			},
+			*/
 			Check: acceptance.ComposeTestCheckFunc(
 				check.That(data.ResourceName).ExistsInAzure(r),
 				check.That(data.ResourceName).Key("execution_frequency_in_seconds").HasValue("20"),
@@ -93,7 +93,7 @@ resource "azurerm_automation_watcher" "test" {
   }
 
   script_parameters = {
-    param_foo = "arg_bar"
+    foo = "bar"
   }
 
   script_run_on                  = azurerm_automation_hybrid_runbook_worker_group.test.name
