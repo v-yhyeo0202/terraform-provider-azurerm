df2e9de34699f6364d076c2a5a00c06f9461454d
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-27 11:44:27 +0800

    Draft

--- internal/services/containerapps/container_app_environment_resource.go
+++ internal/services/containerapps/container_app_environment_resource.go
@@ -421,7 +421,7 @@ func (r ContainerAppEnvironmentResource) Read() sdk.ResourceFunc {
 					state.WorkloadProfiles = helpers.FlattenWorkloadProfiles(props.WorkloadProfiles)
 					state.InfrastructureResourceGroup = pointer.From(props.InfrastructureResourceGroup)
 					state.Mtls = pointer.From(props.PeerAuthentication.Mtls.Enabled)
-					old, new := metadata.ResourceDiff.GetChange("workload_profiles")
+					old, new := metadata.ResourceData.GetChange("workload_profiles")
 					fmt.Println("debug0", old, new)
 					if rawWorkloadProfiles, ok := metadata.ResourceData.GetOk("workload_profiles"); ok {
 						fmt.Println("debug1")
