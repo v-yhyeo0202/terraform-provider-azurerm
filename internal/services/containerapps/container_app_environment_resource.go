1a0497b3fee88dd7cf8869ca9864d2e0d86dc9e1
Ying Heng Yeo (Centific Technologies Inc) <v-yyeo@microsoft.com>
2026-01-29 21:16:28 +0800

    Draft

--- internal/services/containerapps/container_app_environment_resource.go
+++ internal/services/containerapps/container_app_environment_resource.go
@@ -417,7 +417,7 @@ func (r ContainerAppEnvironmentResource) Read() sdk.ResourceFunc {
 					state.DefaultDomain = pointer.From(props.DefaultDomain)
 					state.InfrastructureResourceGroup = pointer.From(props.InfrastructureResourceGroup)
 					state.Mtls = pointer.From(props.PeerAuthentication.Mtls.Enabled)
-					fmt.Println("debug5")
+					fmt.Println("debug5", len(state.WorkloadProfiles), len(existingState.WorkloadProfiles), state.WorkloadProfiles[0].WorkloadProfileType)
 					if !(len(state.WorkloadProfiles) == 1 && len(existingState.WorkloadProfiles) == 0 && state.WorkloadProfiles[0].WorkloadProfileType == string(helpers.WorkloadProfileSkuConsumption)) {
 						fmt.Println("debug6")
 						state.WorkloadProfiles = helpers.FlattenWorkloadProfiles(props.WorkloadProfiles)
