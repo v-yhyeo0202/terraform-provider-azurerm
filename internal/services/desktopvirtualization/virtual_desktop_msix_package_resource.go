e5f6491539175df653a9c4b8b00ac7a53ea347fa
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-05-22 18:27:39 +0800

    Draft

---
+++ internal/services/desktopvirtualization/virtual_desktop_msix_package_resource.go
@@ -0,0 +1,85 @@
+// Copyright IBM Corp. 2014, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package desktopvirtualization
+
+import (
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonschema"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/desktopvirtualization/2025-10-10/msixpackage"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
+)
+
+var (
+	_ sdk.Resource           = VirtualDesktopMsixPackageResource{}
+	_ sdk.ResourceWithUpdate = VirtualDesktopMsixPackageResource{}
+)
+
+type VirtualDesktopMsixPackageResource struct{}
+
+type VirtualDesktopMsixPackageResourceModel struct {
+	Name                  string `tfschema:"name"`
+	ResourceGroupName     string `tfschema:"resource_group_name"`
+	HostPoolName          string `tfschema:"host_pool_name"`
+	ImagePath             string `tfschema:"image_path"`
+	DisplayName           string `tfschema:"display_name"`
+	IsRegularRegistration bool   `tfschema:"is_regular_registration"`
+	IsActive              bool   `tfschema:"is_active"`
+}
+
+func (r VirtualDesktopMsixPackageResource) ModelObject() interface{} {
+	return &VirtualDesktopMsixPackageResourceModel{}
+}
+
+func (r VirtualDesktopMsixPackageResource) IDValidationFunc() pluginsdk.SchemaValidateFunc {
+	return msixpackage.ValidateMsixPackageID
+}
+
+func (r VirtualDesktopMsixPackageResource) ResourceType() string {
+	return "azurerm_virtual_desktop_msix_package"
+}
+
+func (r VirtualDesktopMsixPackageResource) Arguments() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{
+		"name": {
+			Type:         pluginsdk.TypeString,
+			Required:     true,
+			ForceNew:     true,
+			ValidateFunc: validation.StringIsNotEmpty,
+		},
+
+		"resource_group_name": commonschema.ResourceGroupName(),
+
+		"host_pool_name": {
+			Type:     pluginsdk.TypeString,
+			Required: true,
+			ForceNew: true,
+		},
+
+		"image_path": {
+			Type:     pluginsdk.TypeString,
+			Optional: true,
+			ForceNew: true,
+		},
+
+		"display_name": {
+			Type:     pluginsdk.TypeString,
+			Optional: true,
+		},
+
+		"is_regular_registration": {
+			Type:     pluginsdk.TypeBool,
+			Optional: true,
+		},
+
+		"is_active": {
+			Type:     pluginsdk.TypeBool,
+			Optional: true,
+		},
+	}
+}
+
+func (r VirtualDesktopMsixPackageResource) Attributes() map[string]*pluginsdk.Schema {
+	return map[string]*pluginsdk.Schema{}
+}
