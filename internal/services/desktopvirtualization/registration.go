f5e8e1c7211a99cb9961c5914cf20755353ef952
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-06-02 11:37:48 +0800

    Draft

--- internal/services/desktopvirtualization/registration.go
+++ internal/services/desktopvirtualization/registration.go
@@ -41,7 +41,9 @@ func (r Registration) DataSources() []sdk.DataSource {
 }
 
 func (r Registration) Resources() []sdk.Resource {
-	return []sdk.Resource{}
+	return []sdk.Resource{
+		VirtualDesktopAppAttachPackageResource{},
+	}
 }
 
 // SupportedDataSources returns the supported Data Sources supported by this Service
