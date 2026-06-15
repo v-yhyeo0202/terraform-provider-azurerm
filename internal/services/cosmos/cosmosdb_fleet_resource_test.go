cc4a7b8065a1aaa39e3dbd45cf2511aa94738cda
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-06-15 10:44:13 +0800

    Draft

--- internal/services/cosmos/cosmosdb_fleet_resource_test.go
+++ internal/services/cosmos/cosmosdb_fleet_resource_test.go
@@ -33,6 +33,36 @@ func TestAccCosmosdbFleet_basic(t *testing.T) {
 	})
 }
 
+func TestAccCosmosdbFleet_complete(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_cosmosdb_fleet", "test")
+	r := CosmosdbFleetResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.complete(data),
+			Check: acceptance.ComposeAggregateTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.ImportStep(),
+	})
+}
+
+func TestAccCosmosdbFleet_requiresImport(t *testing.T) {
+	data := acceptance.BuildTestData(t, "azurerm_cosmosdb_fleet", "test")
+	r := CosmosdbFleetResource{}
+
+	data.ResourceTest(t, r, []acceptance.TestStep{
+		{
+			Config: r.basic(data),
+			Check: acceptance.ComposeAggregateTestCheckFunc(
+				check.That(data.ResourceName).ExistsInAzure(r),
+			),
+		},
+		data.RequiresImportErrorStep(r.requiresImport),
+	})
+}
+
 func (r CosmosdbFleetResource) Exists(ctx context.Context, clients *clients.Client, state *pluginsdk.InstanceState) (*bool, error) {
 	id, err := fleets.ParseFleetID(state.ID)
 	if err != nil {
@@ -57,6 +87,34 @@ resource "azurerm_cosmosdb_fleet" "test" {
   location            = azurerm_resource_group.test.location
 }
 `, r.template(data), data.RandomInteger)
+}
+
+func (r CosmosdbFleetResource) complete(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_cosmosdb_fleet" "test" {
+  name                = "acctest-fleet-%d"
+  resource_group_name = azurerm_resource_group.test.name
+  location            = azurerm_resource_group.test.location
+
+  tags = {
+    environment = "Production"
+  }
+}
+`, r.template(data), data.RandomInteger)
+}
+
+func (r CosmosdbFleetResource) requiresImport(data acceptance.TestData) string {
+	return fmt.Sprintf(`
+%s
+
+resource "azurerm_cosmosdb_fleet" "import" {
+  name                = azurerm_cosmosdb_fleet.test.name
+  resource_group_name = azurerm_cosmosdb_fleet.test.resource_group_name
+  location            = azurerm_cosmosdb_fleet.test.location
+}
+`, r.basic(data))
 }
 
 func (r CosmosdbFleetResource) template(data acceptance.TestData) string {
