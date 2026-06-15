c52e4a04169d60d9252dc45ee4a7eb03b7cc1a27
v-yhyeo0202 <v-yyeo@microsoft.com>
2026-06-15 15:30:59 +0800

    Draft

---
+++ internal/services/cosmos/cosmosdb_fleet_resource_list.go
@@ -0,0 +1,98 @@
+// Copyright IBM Corp. 2014, 2025
+// SPDX-License-Identifier: MPL-2.0
+
+package cosmos
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/hashicorp/go-azure-helpers/lang/pointer"
+	"github.com/hashicorp/go-azure-helpers/resourcemanager/commonids"
+	"github.com/hashicorp/go-azure-sdk/resource-manager/cosmosdb/2025-10-15/fleets"
+	"github.com/hashicorp/terraform-plugin-framework/list"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/sdk"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
+)
+
+type CosmosDbFleetListResource struct{}
+
+var _ sdk.FrameworkListWrappedResource = new(CosmosDbFleetListResource)
+
+func (CosmosDbFleetListResource) Metadata(_ context.Context, _ resource.MetadataRequest, response *resource.MetadataResponse) {
+	response.TypeName = CosmosDbFleetResource{}.ResourceType()
+}
+
+func (CosmosDbFleetListResource) ResourceFunc() *pluginsdk.Resource {
+	return sdk.WrappedResource(CosmosDbFleetResource{})
+}
+
+func (CosmosDbFleetListResource) List(ctx context.Context, request list.ListRequest, stream *list.ListResultsStream, metadata sdk.ResourceMetadata) {
+	client := metadata.Client.Cosmos.FleetsClient
+
+	var data sdk.DefaultListModel
+	diags := request.Config.Get(ctx, &data)
+	if diags.HasError() {
+		stream.Results = list.ListResultsStreamDiagnostics(diags)
+		return
+	}
+
+	var results []fleets.FleetResource
+	subscriptionID := metadata.SubscriptionId
+	if !data.SubscriptionId.IsNull() {
+		subscriptionID = data.SubscriptionId.ValueString()
+	}
+
+	fleetResource := CosmosDbFleetResource{}
+
+	switch {
+	case !data.ResourceGroupName.IsNull():
+		resp, err := client.FleetListByResourceGroupComplete(ctx, commonids.NewResourceGroupID(subscriptionID, data.ResourceGroupName.ValueString()))
+		if err != nil {
+			sdk.SetResponseErrorDiagnostic(stream, fmt.Sprintf("listing `%s`", fleetResource.ResourceType()), err)
+			return
+		}
+
+		results = resp.Items
+	default:
+		resp, err := client.FleetListComplete(ctx, commonids.NewSubscriptionID(subscriptionID))
+		if err != nil {
+			sdk.SetResponseErrorDiagnostic(stream, fmt.Sprintf("listing `%s`", fleetResource.ResourceType()), err)
+			return
+		}
+
+		results = resp.Items
+	}
+
+	stream.Results = func(push func(list.ListResult) bool) {
+		for _, item := range results {
+			result := request.NewListResult(ctx)
+			result.DisplayName = pointer.From(item.Name)
+
+			id, err := fleets.ParseFleetIDInsensitively(pointer.From(item.Id))
+			if err != nil {
+				sdk.SetErrorDiagnosticAndPushListResult(result, push, "parsing Fleet ID", err)
+				return
+			}
+
+			meta := sdk.NewResourceMetaData(metadata.Client, fleetResource)
+			meta.SetID(id)
+
+			if err := fleetResource.flatten(meta, id, &item); err != nil {
+				sdk.SetErrorDiagnosticAndPushListResult(result, push, fmt.Sprintf("encoding `%s` resource data", fleetResource.ResourceType()), err)
+				return
+			}
+
+			sdk.EncodeListResult(ctx, meta.ResourceData, &result)
+			if result.Diagnostics.HasError() {
+				push(result)
+				return
+			}
+
+			if !push(result) {
+				return
+			}
+		}
+	}
+}
